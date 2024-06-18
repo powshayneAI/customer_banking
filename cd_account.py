ed6ebf233cca0a29ee908e53cf231d9528f16337
powshayneAI <166059256+powshayneAI@users.noreply.github.com>
2024-06-18 10:03:08 -0700

    ore progress i think

---
+++ cd_account.py
@@ -0,0 +1,15 @@
+from Accounts import Account
+
+create_cd_account(balance, interest):
+    account_instance =  Account(balance, interest)
+    interest_earned = balance * (interest / 100)
+
+    account_instance.set_balance(balance)
+    account_instance.set_interest(0)
+
+    new_balance = balance + interest_earned
+
+    account_instance.set_balance(new_balance)
+    account_instance.set_interest(interest_earned)
+
+    return new_balance, interest_earned
--- savings_account.py
+++ savings_account.py
@@ -1,13 +1,15 @@
 from Accounts import Account
 
-create_savings_account(balance, interest_rate):
-    account_instance = Account(balance, interest_rate)
-    interest = balance * (interest_rate / 100)
+create_savings_account(balance, interest):
+    account_instance = Account(balance, interest)
+    interest_earned = balance * (interest / 100)
+
+    account_instance.set_balance(balance)
+    account_instance.set_interest(0)
+
+    new_balance = balance + interest_earned
 
     account_instance.set_balance(new_balance)
+    account_instance.set_interest(interest_earned)
 
-    account_instance.set_interest(interest)
-
-    new_balance = balance + interest
-
-    return new_balance, interest
+    return new_balance, interest_earned
